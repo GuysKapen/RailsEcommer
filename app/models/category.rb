618d4e6f22b85788e292a2a89bcb95dfa1a91a99
GuysKapen <guyskapen@gmail.com>
2021-09-12 15:15:08 -0800

    Add validations for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Category < ApplicationRecord
   belongs_to :user
   has_many :products
+
+  validates :name, presence: true, allow_blank: false,
+                   uniqueness: { scope: :user_id, message: 'Category already exist' }
 end
