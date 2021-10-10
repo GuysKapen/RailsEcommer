9dafbfff7c796c424d4d281663cfece4c2cfd1ae
GuysKapen <guyskapen@gmail.com>
2021-10-10 14:34:19 -0800

    Clean, fix product category and add link to filter with category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@
 
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :products
+  has_and_belongs_to_many :products
 
   validates :name, presence: true, allow_blank: false,
                    uniqueness: { scope: :user_id, message: 'Category already exist' }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_231203) do
+ActiveRecord::Schema.define(version: 2021_10_10_221231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -253,8 +253,6 @@ ActiveRecord::Schema.define(version: 2021_08_28_231203) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
