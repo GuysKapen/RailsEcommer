669d275e34b4d4496d8083bc4c9beb40b4bfaf41
GuysKapen <guyskapen@gmail.com>
2022-01-04 15:07:05 -0900

    Add save check DOM find

--- app/javascript/packs/utils/setup.js
+++ app/javascript/packs/utils/setup.js
@@ -3,7 +3,7 @@ window.setupInputQty = function () {
         document.getElementById("container-new-category").style.display = "inherit"
     })
 
-    document.getElementById("btn-add-new-category").addEventListener("click", function () {
+    document.getElementById("btn-add-new-category")?.addEventListener("click", function () {
         const category_name = document.getElementById("input_new_category").value
         Rails.ajax({
             url: "/products/add_category",
@@ -45,51 +45,6 @@ window.setupInputQty = function () {
 window.setupInputVariation = function setupInputVariation() {
 
     setupFormAddToCart(window.location.pathname.split('/').pop())
-    // // Get inputs hidden options for variations
-    // const inputsHidden = document.getElementsByClassName("input-add-to-cart-hidden")
-    //
-    // for (let i = 0; i < inputsHidden.length; i++) {
-    //     const name = inputsHidden[i].attributes['name'].value
-    //     // Get all options for attrs have name
-    //     const inputs = document.getElementsByClassName("input-add-to-cart-" + name + "-select")
-    //
-    //     for (let j = 0; j < inputs.length; j++) {
-    //         inputs[j].addEventListener("click", function () {
-    //             // Remove active from all other
-    //             for (let k = 0; k < inputs.length; k++) {
-    //                 if (inputs[j] !== inputs[k]) {
-    //                     inputs[k].classList.remove("custom-select-active")
-    //                 } else {
-    //                     inputs[k].classList.toggle("custom-select-active")
-    //                 }
-    //             }
-    //
-    //             inputsHidden[i].value = inputs[j].attributes['data-value'].value
-    //
-    //             const values = []
-    //             for (let k = 0; k < inputsHidden.length; k++) {
-    //                 values.push(inputsHidden[k].value)
-    //             }
-    //             Rails.ajax({
-    //                 url: `/products/${}/update_form_add_to_cart`,
-    //                 type: "post",
-    //                 beforeSend(xhr, options) {
-    //                     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
-    //                     // Workaround: add options.data late to avoid Content-Type header to already being set in stone
-    //                     // https://github.com/rails/rails/blob/master/actionview/app/assets/javascripts/rails-ujs/utils/ajax.coffee#L53
-    //                     options.data = JSON.stringify({"values": values})
-    //                     return true
-    //                 },
-    //                 success: function () {
-    //
-    //                 },
-    //                 error: function () {
-    //
-    //                 }
-    //             })
-    //         })
-    //     }
-    // }
 
 }
 window.setupFormAddToCart = function setupFormAddToCart(productId) {
